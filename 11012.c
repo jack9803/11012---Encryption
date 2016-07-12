0da4b3f5a754fad3bc8636cdd0dcd6408d7af410
jack9803 <j29285357@gmail.com>
2016-07-12 22:41:05 +0800

    Update 11012.c

--- 11012.c
+++ 11012.c
@@ -2,9 +2,9 @@
 #include<stdlib.h>
 #include<string.h>
 
-char S[1000];
-char A[40][40]={'\0'};
-char ans[1600]={'\0'};
+char S[1010];
+char A[40][40];
+char ans[1600];
 int valid[40][40];
 int main(){
 char tmp;
@@ -13,19 +13,38 @@ int count=0;
 int i,j,width;
 int row_cur,col_cur,num,dir;
 int count_ans;
-for(i=0;i<40;i++){
+        for(i=0;i<40;i++){
+            for(j=0;j<40;j++){
+                A[i][j]=' ';
+            }
+            }
+        for(i=0;i<40;i++){
             for(j=0;j<40;j++){
                 valid[i][j]=1;
             }
-}
+        }
+        for(i=0;i<1600;i++){
+            ans[i]=' ';
+        }
+        for(i=0;i<1010;i++){
+            S[i]=' ';
+        }
+
+
 while(1){
+
+
     tmp=getchar();
     if(tmp=='\n'&&start==0){
         printf("\n");
-    }else if((tmp=='\n'||tmp==EOF)&&start==1){
-        //printf("%s\n",S);
+    }
+    else if (tmp==EOF) {
+        break;
+    }
+    else if((tmp=='\n')&&start==1){
+
         for(i=0;i<40;i++){
-            if((i*i)>count){
+            if((i*i)>=count){
                 width=i;
                 break;
             }
@@ -35,24 +54,14 @@ while(1){
                 valid[i][j]=0;
             }
         }
-        //yayayayyayayaya
-        /*for(i=0;i<40;i++){
-            for(j=0;j<40;j++){
-                printf("%d",valid[i][j]);
-            }
-            printf("\n");
-        }*/
-        //printf("%s\n",S);
+
         A[0][0]=S[0];
         row_cur=0;
         col_cur=0;
         valid[0][0]=1;
         dir=0;
         num=1;
-//printf("num: %d count:%d\n",num,count);
-    while(num<=count){
-
-
+    while(num<count){
         if(dir%4==0){
             while(valid[row_cur+1][col_cur]==0){
                 row_cur++;
@@ -62,7 +71,6 @@ while(1){
             }
             dir++;
         }
-        //printf("num: %d dir: %d count:%d\n",num,dir,count);
         if(dir%4==1){
             while(valid[row_cur][col_cur+1]==0){
                 col_cur++;
@@ -93,12 +101,6 @@ while(1){
             dir++;
         }
     }
-        /*for(i=0;i<width;i++){
-            for(j=0;j<width;j++){
-                printf("%c",A[i][j]);
-            }
-                printf("\n");
-        }*/
         count_ans=0;
         for(i=0;i<width;i++){
             for(j=0;j<width;j++){
@@ -106,32 +108,42 @@ while(1){
                 count_ans++;
             }
         }
-        for(j=0;j<count_ans;j++){
-            printf("%c",ans[j]);
+        for(j=count_ans-1;j>=0;j--){
+            if(ans[j]!=' '){
+                ans[j+1]='\0';
+                printf("%s",ans);
+                j=-1;
+            }
         }
-        printf("\n");
+
+            printf("\n");
+
         /*intitialize*/
         start=0;
         count=0;
-        memset(S,'\0',1000);
-        memset(A,'\0',1600);
-        memset(ans,'\0',1600);
+        for(i=0;i<1600;i++){
+            ans[i]=' ';
+        }
+        for(i=0;i<1010;i++){
+            S[i]=' ';
+        }
         width=0;
         for(i=0;i<40;i++){
             for(j=0;j<40;j++){
                 valid[i][j]=1;
             }
         }
+        for(i=0;i<40;i++){
+            for(j=0;j<40;j++){
+                A[i][j]=' ';
+            }
+            }
         /*intitialize*/
-        if(tmp==EOF){
-            break;
-        }
     }else{
         start=1;
         S[count]=tmp;
         count++;
     }
 }
-
 return 0;
 }
