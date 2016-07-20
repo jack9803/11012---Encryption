c8f61cebc7d806cdcf66e01626cd994fc1238c72
YU-PU HUANG <j29285357@gmail.com>
2016-07-20 15:41:06 +0800

    Update 11012.c
    better one

--- 11012.c
+++ 11012.c
@@ -4,145 +4,133 @@
 
 char S[1010];
 char A[40][40];
-char ans[1600];
-int valid[40][40];
+int visit[40][40];
+char ans[1010];
+
 int main(){
+
 char tmp;
 int start=0;
-int count=0;
-int i,j,width;
-int row_cur,col_cur,num,dir;
-int count_ans;
-        for(i=0;i<40;i++){
-            for(j=0;j<40;j++){
-                A[i][j]=' ';
-            }
-            }
-        for(i=0;i<40;i++){
-            for(j=0;j<40;j++){
-                valid[i][j]=1;
-            }
-        }
-        for(i=0;i<1600;i++){
-            ans[i]=' ';
-        }
-        for(i=0;i<1010;i++){
-            S[i]=' ';
-        }
+int num_s=0;
+int width,dir,cur,row_cur,col_cur;
+int c_ans;
+int i,j;
 
+for(i=0;i<40;i++){
+    for(j=0;j<40;j++){
+        visit[i][j]=1;
+        A[i][j]=' ';
+    }
+}
+for(i=0;i<1010;i++){
+    S[i]=' ';
+    ans[i]=' ';
+}
 
 while(1){
-
-
     tmp=getchar();
-    if(tmp=='\n'&&start==0){
-        printf("\n");
-    }
-    else if (tmp==EOF) {
-        break;
-    }
-    else if((tmp=='\n')&&start==1){
 
+    if(start==0&&tmp=='\n'){
+        printf("\n");
+    }else if(tmp==EOF){
+        break;
+    }else if(tmp!='\n'){
+        start=1;
+        S[num_s]=tmp;
+        num_s++;
+    }else if(start==1&&tmp=='\n'){
+
+        S[num_s]='\0';
+        //printf("%s\n",S);
         for(i=0;i<40;i++){
-            if((i*i)>=count){
+            if(i*i>=num_s){
                 width=i;
                 break;
             }
         }
         for(i=0;i<width;i++){
             for(j=0;j<width;j++){
-                valid[i][j]=0;
+                visit[i][j]=0;
             }
         }
-
+        S[num_s]=' ';
         A[0][0]=S[0];
+        visit[0][0]=1;
         row_cur=0;
         col_cur=0;
-        valid[0][0]=1;
         dir=0;
-        num=1;
-    while(num<count){
-        if(dir%4==0){
-            while(valid[row_cur+1][col_cur]==0){
-                row_cur++;
-                A[row_cur][col_cur]=S[num];
-                num++;
-                valid[row_cur][col_cur]=1;
+        cur=1;
+        while(cur<num_s){
+
+            if((dir%4)==0){
+                while(visit[row_cur+1][col_cur]==0){
+                    row_cur++;
+                    A[row_cur][col_cur]=S[cur];
+                    visit[row_cur][col_cur]=1;
+                    cur++;
+                }
+                dir++;
             }
-            dir++;
-        }
-        if(dir%4==1){
-            while(valid[row_cur][col_cur+1]==0){
-                col_cur++;
-                A[row_cur][col_cur]=S[num];
-                num++;
-                valid[row_cur][col_cur]=1;
+
+            if((dir%4)==1){
+                while(visit[row_cur][col_cur+1]==0){
+                    col_cur++;
+                    A[row_cur][col_cur]=S[cur];
+                    visit[row_cur][col_cur]=1;
+                    cur++;
+                }
+                dir++;
             }
-            dir++;
-        }
-        if(dir%4==2){
-            while(valid[row_cur-1][col_cur]==0
-                  &&row_cur-1>=0){
-                row_cur--;
-                A[row_cur][col_cur]=S[num];
-                num++;
-                valid[row_cur][col_cur]=1;
+
+            if((dir%4)==2){
+                while(visit[row_cur-1][col_cur]==0&&
+                      row_cur-1>=0){
+                    row_cur--;
+                    A[row_cur][col_cur]=S[cur];
+                    visit[row_cur][col_cur]=1;
+                    cur++;
+                }
+                dir++;
             }
-            dir++;
-        }
-        if(dir%4==3){
-            while(valid[row_cur][col_cur-1]==0
-                  &&col_cur-1>=0){
-                col_cur--;
-                A[row_cur][col_cur]=S[num];
-                num++;
-                valid[row_cur][col_cur]=1;
+
+            if((dir%4)==3){
+                while(visit[row_cur][col_cur-1]==0&&
+                      col_cur-1>=0){
+                    col_cur--;
+                    A[row_cur][col_cur]=S[cur];
+                    visit[row_cur][col_cur]=1;
+                    cur++;
+                }
+                dir++;
             }
-            dir++;
         }
-    }
-        count_ans=0;
+        c_ans=0;
         for(i=0;i<width;i++){
             for(j=0;j<width;j++){
-                ans[count_ans]=A[i][j];
-                count_ans++;
+                ans[c_ans]=A[i][j];
+                c_ans++;
             }
         }
-        for(j=count_ans-1;j>=0;j--){
-            if(ans[j]!=' '){
-                ans[j+1]='\0';
-                printf("%s",ans);
-                j=-1;
+        for(i=c_ans-1;i>=0;i--){
+            if(ans[i]!=' '){
+                ans[i+1]='\0';
+                break;
             }
         }
-
-            printf("\n");
-
-        /*intitialize*/
+        printf("%s\n",ans);
+        /*initialize*/
         start=0;
-        count=0;
-        for(i=0;i<1600;i++){
-            ans[i]=' ';
+        num_s=0;
+        for(i=0;i<40;i++){
+            for(j=0;j<40;j++){
+                visit[i][j]=1;
+                A[i][j]=' ';
+            }
         }
         for(i=0;i<1010;i++){
             S[i]=' ';
+            ans[i]=' ';
         }
-        width=0;
-        for(i=0;i<40;i++){
-            for(j=0;j<40;j++){
-                valid[i][j]=1;
-            }
-        }
-        for(i=0;i<40;i++){
-            for(j=0;j<40;j++){
-                A[i][j]=' ';
-            }
-            }
-        /*intitialize*/
-    }else{
-        start=1;
-        S[count]=tmp;
-        count++;
     }
 }
 return 0;
